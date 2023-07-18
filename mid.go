b13977e98a5a9d5f4f5bd5183fb60383ccce8b8e
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-07-18 09:49:34 +0200

    add calMintFuntion

--- mid.go
+++ mid.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -39,4 +40,34 @@ func NewMasaIdentity(client *ethclient.Client, contractAddress, abiJSON string) 
 // Mint calls the "mint" function on the Masa Identity contract.
 func (mi *MasaIdentity) Mint(opts *bind.TransactOpts, to common.Address, tokenId int) (*types.Transaction, error) {
 	return mi.contract.Mint(opts, to, big.NewInt(int64(tokenId)))
+}
+
+func callMintFunction() error {
+	// Set up Ethereum client
+	client, err := ethclient.Dial("https://mainnet.infura.io")
+	if err != nil {
+		return err
+	}
+
+	// Load the contract
+	mi, err := NewMasaIdentity(client, "YourContractAddress", "YourABI")
+	if err != nil {
+		return err
+	}
+
+	// Set up transaction options
+	privateKey, err := crypto.HexToECDSA("YourPrivateKey")
+	if err != nil {
+		return err
+	}
+	auth := bind.NewKeyedTransactor(privateKey)
+
+	// Call Mint function
+	toAddress := common.HexToAddress("AddressToMintTo")
+	_, err = mi.Mint(auth, toAddress, 1234) // replace 1234 with your tokenId
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
