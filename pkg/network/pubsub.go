d856c00e0dba23b4aebace9903fd6de6b376c70a
Bob Stevens <35038919+restevens402@users.noreply.github.com>
2023-09-27 09:13:10 -0700

    Gossip (#24)
    * initial check in to distribute to other machines
    
    * always send messages for testing
    
    ignore messages sent by the same node
    
    * extra logging, looks good
    
    * adding more protocols to support the javascript libp2p

--- pkg/network/pubsub.go
+++ pkg/network/pubsub.go
@@ -1,15 +1,71 @@
 package network
 
 import (
+	"bufio"
 	"context"
+	"fmt"
+	"os"
 
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/multiformats/go-multiaddr"
+	"github.com/sirupsen/logrus"
 )
 
-func PubSub(ctx context.Context, host host.Host) error {
-	//gossipSub, err := pubsub.NewGossipSub(ctx, host)
-	//if err != nil {
-	//	return err
-	//}
-	return nil
+func NewPubSub(ctx context.Context, host host.Host, topicName string) (*pubsub.Topic, error) {
+	gossipSub, err := pubsub.NewGossipSub(ctx, host)
+	if err != nil {
+		return nil, err
+	}
+
+	// Subscribe to a topic
+	topic, err := gossipSub.Join(topicName)
+	if err != nil {
+		return nil, err
+	}
+	go streamConsoleTo(ctx, topic)
+
+	sub, err := topic.Subscribe()
+	if err != nil {
+		panic(err)
+	}
+
+	// Read messages from the subscription
+	go func() {
+		for {
+			m, err := sub.Next(ctx)
+			if err != nil {
+				logrus.Error(err)
+			}
+			// Skip messages from the same node
+			if m.ReceivedFrom == host.ID() {
+				continue
+			}
+			// Get the peer's IP address
+			var addrs multiaddr.Multiaddr
+			connectedness := host.Network().Connectedness(m.ReceivedFrom)
+			if connectedness == network.Connected {
+				peerInfo := host.Peerstore().PeerInfo(m.ReceivedFrom)
+				addrs = peerInfo.Addrs[0]
+				logrus.Infof("%s : %s : %s", m.ReceivedFrom, string(m.Message.Data), addrs.String())
+			} else {
+				logrus.Info(m.ReceivedFrom, ": ", string(m.Message.Data))
+			}
+		}
+	}()
+	return topic, nil
+}
+
+func streamConsoleTo(ctx context.Context, topic *pubsub.Topic) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			logrus.Error(err)
+		}
+		if err := topic.Publish(ctx, []byte(s)); err != nil {
+			fmt.Println("### Publish error:", err)
+		}
+	}
 }
