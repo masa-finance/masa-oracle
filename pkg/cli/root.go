656eed1af9782876ba93ba290ac9156378b4f8cc
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-11-24 13:18:56 -0800

    Update root.go

--- pkg/cli/root.go
+++ pkg/cli/root.go
@@ -2,23 +2,22 @@ package cli
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "masa",
-	Short: "Masa is a blockchain oracle",
-	Long:  `A blockchain oracle built with love by Masa Finance.`,
+var bootnodes = "/ip4/137.66.11.250/udp/4001/quic-v1/p2p/16Uiu2HAmJtYy4A8pzChDQQLrPsu1SQU5apzCftCVaAjFk539CLc9,/ip4/168.220.95.86/udp/4001/quic-v1/p2p/16Uiu2HAmHgacSbefXenfMnoDNqv9sDjzGCuCgZa1zoJCC7CBXNka,/ip4/35.224.231.145/udp/4001/quic-v1/p2p/16Uiu2HAm47nBiewWLLzCREtY8vwPQtr5jTqyrEoUo6WnngwhsQuR,/ip4/104.198.43.138/udp/4001/quic-v1/p2p/16Uiu2HAkxiP8jjdHQWeCxTr7pD6BvoPkS8Z1skjCy9vdSRMACDcc"
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start the Masa node",
+	Long:  `Start the Masa node with predefined bootnodes.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+		fmt.Printf("Starting the Masa node with bootnodes: %s\n", bootnodes)
+		// Add your code to start the node here
 	},
 }
 
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+func init() {
+	rootCmd.AddCommand(startCmd)
 }
