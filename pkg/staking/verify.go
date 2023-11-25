21c24fb105638137e0755a3786c7be9cf926dc4d
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-11-24 23:22:38 -0800

    Update to take stake event from contract

--- pkg/staking/verify.go
+++ pkg/staking/verify.go
@@ -1,29 +1,51 @@
 package staking
 
 import (
-	"crypto/ecdsa"
-	"crypto/sha256"
-	"crypto/x509"
-	"encoding/pem"
+	"context"
+	"log"
 	"math/big"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func VerifyStakingSignature(signature, pubKey, data string) bool {
-	// Convert the public key to an ecdsa.PublicKey
-	block, _ := pem.Decode([]byte(pubKey))
-	pubInterface, _ := x509.ParsePKIXPublicKey(block.Bytes)
-	pub := pubInterface.(*ecdsa.PublicKey)
+const (
+	infuraURL       = "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID"
+	contractAddress = "0xYourContractAddress"
+	contractABI     = `[{"constant":true,"inputs":[],"name":"stakes","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"user","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"Staked","type":"event"}]`
+)
 
-	// Convert the signature to r and s values
-	r := new(big.Int)
-	s := new(big.Int)
-	sigLen := len(signature)
-	r.SetBytes([]byte(signature[:sigLen/2]))
-	s.SetBytes([]byte(signature[sigLen/2:]))
+func VerifyStakingEvent(userAddress string) bool {
+	client, err := ethclient.Dial(infuraURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
 
-	// Verify the signature
-	hash := sha256.Sum256([]byte(data))
-	isValid := ecdsa.Verify(pub, hash[:], r, s)
+	contractAbi, err := abi.JSON(strings.NewReader(contractABI))
+	if err != nil {
+		log.Fatalf("Failed to parse contract ABI: %v", err)
+	}
 
-	return isValid
+	address := common.HexToAddress(userAddress)
+	callOpts := &bind.CallOpts{}
+	stake, err := contractAbi.Pack("stakes", address)
+	if err != nil {
+		log.Fatalf("Failed to pack data for stakes call: %v", err)
+	}
+
+	result, err := client.CallContract(context.Background(), callOpts, contractAddress, stake)
+	if err != nil {
+		log.Fatalf("Failed to call stakes function: %v", err)
+	}
+
+	stakes := new(big.Int)
+	err = contractAbi.Unpack(stakes, "stakes", result)
+	if err != nil {
+		log.Fatalf("Failed to unpack stakes: %v", err)
+	}
+
+	return stakes.Cmp(big.NewInt(0)) > 0
 }
