1616ffa008a162755cbd633ea9da920620c819e5
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-11-24 23:03:48 -0800

    :white_check_mark: working signature check and start node as publisher :rocket:

--- pkg/oracle_node_test.go
+++ pkg/oracle_node_test.go
@@ -1,19 +1,41 @@
 package masa
 
-import "testing"
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/hex"
+	"testing"
 
-func TestIsPublisher(t *testing.T) {
-	node := &OracleNode{
-		Stake:      101,
-		Reputation: 101,
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func TestNodeSignature(t *testing.T) {
+	// Generate a new private key
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
 	}
 
+	// Create a data string
+	data := "This is some test data"
+
+	// Hash the data
+	hash := crypto.Keccak256Hash([]byte(data))
+
+	// Sign the hash with the private key
+	sig, err := crypto.Sign(hash.Bytes(), privateKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a new OracleNode
+	node := &OracleNode{
+		Signature: hex.EncodeToString(sig),
+	}
+
+	// Check if the node is a publisher
 	if !node.IsPublisher() {
 		t.Errorf("Expected node to be a publisher, but it's not")
-	}
-
-	node.Stake = 50
-	if node.IsPublisher() {
-		t.Errorf("Expected node not to be a publisher, but it is")
 	}
 }
