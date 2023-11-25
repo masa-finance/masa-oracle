da2aa40837fc32bde7f5eed361ff12e2d48b1be9
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-11-24 23:22:38 -0800

    Update to take stake event from contract

--- smart-contracts/OracleNodeStakingContract.sol
+++ smart-contracts/OracleNodeStakingContract.sol
@@ -1,11 +1,25 @@
 pragma solidity ^0.8.0;
 
-contract OracleNodeStakingContract {
-    function stakeTokens(uint256 amount) public {
-        // Logic for staking tokens
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract StakingContract {
+    IERC20 public token;
+    mapping(address => uint256) public stakes;
+
+    event Staked(address indexed user, uint256 amount);
+
+    constructor(address tokenAddress) {
+        token = IERC20(tokenAddress);
     }
 
-    function generateSignature(bytes32 hash, uint8 v, bytes32 r, bytes32 s) public view returns (address) {
-        return ecrecover(hash, v, r, s);
+    function stakeTokens(uint256 amount) public {
+        // Transfer the tokens to this contract
+        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+
+        // Update the user's stake
+        stakes[msg.sender] += amount;
+
+        // Emit the Staked event
+        emit Staked(msg.sender, amount);
     }
 }
