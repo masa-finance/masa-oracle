2ba728deed1a6c5f4ba83b4d33adcce04368b54e
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-11-24 22:22:02 -0800

    Verify a signature that is passed as a --signature flag at node start

--- pkg/staking/verify.go
+++ pkg/staking/verify.go
@@ -2,16 +2,28 @@ package staking
 
 import (
 	"crypto/ecdsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
 	"math/big"
-
-	"github.com/ethereum/go-ethereum/crypto"
 )
 
-func VerifyStakingSignature(signature []byte, pubKey *ecdsa.PublicKey, data []byte) bool {
-	r := new(big.Int).SetBytes(signature[:32])
-	s := new(big.Int).SetBytes(signature[32:])
+func VerifyStakingSignature(signature, pubKey, data string) bool {
+	// Convert the public key to an ecdsa.PublicKey
+	block, _ := pem.Decode([]byte(pubKey))
+	pubInterface, _ := x509.ParsePKIXPublicKey(block.Bytes)
+	pub := pubInterface.(*ecdsa.PublicKey)
 
-	hash := crypto.Keccak256Hash(data)
+	// Convert the signature to r and s values
+	r := new(big.Int)
+	s := new(big.Int)
+	sigLen := len(signature)
+	r.SetBytes([]byte(signature[:sigLen/2]))
+	s.SetBytes([]byte(signature[sigLen/2:]))
 
-	return ecdsa.Verify(pubKey, hash.Bytes(), r, s)
+	// Verify the signature
+	hash := sha256.Sum256([]byte(data))
+	isValid := ecdsa.Verify(pub, hash[:], r, s)
+
+	return isValid
 }
