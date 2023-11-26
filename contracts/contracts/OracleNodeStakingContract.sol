b22acb30b004a763ef8396d02b3270686d18de0d
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-11-26 13:12:37 -0800

    Working in truffle for deploying staking contract

--- contracts/contracts/OracleNodeStakingContract.sol
+++ contracts/contracts/OracleNodeStakingContract.sol
@@ -1,28 +1,36 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./MasaToken.sol"; // Import the MasaToken contract
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./MasaToken.sol";
 
-contract StakingContract {
-    IERC20 public token;
+contract OracleNodeStakingContract is ReentrancyGuard {
+    IERC20 public stakingToken;
+
     mapping(address => uint256) public stakes;
 
     event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
 
-    constructor(MasaToken tokenContract) { // Pass the MasaToken contract as a constructor argument
-        token = IERC20(address(tokenContract)); // Cast the MasaToken contract to an IERC20 interface
+    constructor(address _stakingToken) {
+        stakingToken = IERC20(_stakingToken);
     }
 
-    function stakeTokens(uint256 amount) public {
-        // Transfer the tokens to this contract
-        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
-
-        // Update the user's stake
+    function stake(uint256 amount) external nonReentrant {
         stakes[msg.sender] += amount;
-
-        // Emit the Staked event
+        stakingToken.transferFrom(msg.sender, address(this), amount);
         emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) external nonReentrant {
+        require(stakes[msg.sender] >= amount, "Withdraw amount exceeds staked amount");
+        stakes[msg.sender] -= amount;
+        stakingToken.transfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return stakes[account];
     }
 }
