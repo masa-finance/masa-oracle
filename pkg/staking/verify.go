45a8f155278c251d96baa60e56a358be2ea24368
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-11-26 13:44:24 -0800

    Updated verify.go and confirm build from auto-generated abi - connecting to local ganache instance to check stakes :rocket:
    This is ready for testing

--- pkg/staking/verify.go
+++ pkg/staking/verify.go
@@ -2,12 +2,14 @@ package staking
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"math/big"
 	"strings"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
@@ -15,8 +17,23 @@ import (
 const (
 	infuraURL       = "HTTP://127.0.0.1:7545"
 	contractAddress = "0x767b636793c3399B7a517A6487974Bc474db1e7B"
-	contractABI     = `[{"constant":true,"inputs":[],"name":"stakes","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"user","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"Staked","type":"event"}]`
 )
+
+type Contract struct {
+	ABI string `json:"abi"`
+}
+
+func getContractABI() string {
+	jsonFile, err := ioutil.ReadFile("contracts/build/contracts/OracleNodeStakingContract.json")
+	if err != nil {
+		log.Fatalf("Failed to read contract JSON: %v", err)
+	}
+
+	var contract Contract
+	json.Unmarshal(jsonFile, &contract)
+
+	return contract.ABI
+}
 
 func VerifyStakingEvent(userAddress string) bool {
 	client, err := ethclient.Dial(infuraURL)
@@ -24,28 +41,41 @@ func VerifyStakingEvent(userAddress string) bool {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
 
-	contractAbi, err := abi.JSON(strings.NewReader(contractABI))
+	contractABI := getContractABI()
+	parsedABI, err := abi.JSON(strings.NewReader(contractABI))
 	if err != nil {
 		log.Fatalf("Failed to parse contract ABI: %v", err)
 	}
 
 	address := common.HexToAddress(userAddress)
-	callOpts := &bind.CallOpts{}
-	stake, err := contractAbi.Pack("stakes", address)
+	stake, err := parsedABI.Pack("stakes", address)
 	if err != nil {
 		log.Fatalf("Failed to pack data for stakes call: %v", err)
 	}
 
-	result, err := client.CallContract(context.Background(), callOpts, contractAddress, stake)
+	// Address correction
+	contractAddr := common.HexToAddress(contractAddress)
+
+	callMsg := ethereum.CallMsg{
+		To:   &contractAddr, // Adjusted to use a pointer
+		Data: stake,
+	}
+
+	result, err := client.CallContract(context.Background(), callMsg, nil)
 	if err != nil {
 		log.Fatalf("Failed to call stakes function: %v", err)
 	}
 
-	stakes := new(big.Int)
-	err = contractAbi.Unpack(stakes, "stakes", result)
+	stakesAmountInterfaces, err := parsedABI.Unpack("stakes", result)
 	if err != nil {
 		log.Fatalf("Failed to unpack stakes: %v", err)
 	}
 
-	return stakes.Cmp(big.NewInt(0)) > 0
+	stakesAmount, ok := stakesAmountInterfaces[0].(*big.Int)
+	if !ok {
+		log.Fatalf("Failed to assert type: stakesAmount is not *big.Int")
+	}
+
+	return stakesAmount.Cmp(big.NewInt(0)) > 0
+
 }
