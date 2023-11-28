1782f7addae60627c78a9f821e651f1995c8eea1
Mykhaylo Pylypyshyn <bat9r@protonmail.com>
2023-11-28 14:27:05 +0100

    Add masa_oracle_node_output.env

--- pkg/cicd_helpers/cicd_helpers.go
+++ pkg/cicd_helpers/cicd_helpers.go
@@ -3,10 +3,24 @@ package cicd_helpers
 import (
     "fmt"
     "os"
+    "os/user"
+    "strings"
+    "path/filepath"
+    "github.com/sirupsen/logrus"
+    "log"
 )
 
 func SetEnvVariablesForPipeline (multiAddr string) {
-	os.Setenv("MASA_NODE_MULTIADDRESS", multiAddr)
-    fmt.Println("MASA_NODE_MULTIADDRESS:", os.Getenv("MASA_NODE_MULTIADDRESS"))
+    usr, err := user.Current()
+	if err != nil {
+		log.Fatal("could not find user.home directory")
+	}
+    outputFilePath := filepath.Join(usr.HomeDir, ".masa", "masa_oracle_node_output.env")
+    builder := strings.Builder{}
+	builder.WriteString(fmt.Sprintf("export MASA_NODE_MULTIADDRESS='%s\n'", multiAddr))
+	err = os.WriteFile(outputFilePath, []byte(builder.String()), 0755)
+	if err != nil {
+		logrus.Fatal("could not write to masa_oracle_node_output.env file:", err)
+	}
 }
 
