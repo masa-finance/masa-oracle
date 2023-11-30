4db13567568a7238645b1fdf3b6ce7fa79c541c2
Bob Stevens <35038919+restevens402@users.noreply.github.com>
2023-11-30 15:27:55 -0800

    check in to merge main into this branch

--- pkg/oracle_node_listener.go
+++ pkg/oracle_node_listener.go
@@ -5,6 +5,8 @@ import (
 	"log"
 
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
 )
 
 func (node *OracleNode) ListenToNodeTracker() {
@@ -63,4 +65,52 @@ func (node *OracleNode) SubscribeToTopic() error {
 		}
 	}()
 	return nil
+}
+
+func (node *OracleNode) SendNodeData(peerID peer.ID) {
+	stream, err := node.Host.NewStream(node.Context, peerID, NodeDataSyncProtocol)
+	if err != nil {
+		log.Printf("Failed to open stream to %s: %v", peerID, err)
+		return
+	}
+
+	nodeData := node.NodeTracker.nodeData
+	jsonData, err := json.Marshal(nodeData)
+	if err != nil {
+		log.Printf("Failed to marshal NodeData: %v", err)
+		return
+	}
+
+	_, err = stream.Write(jsonData)
+	if err != nil {
+		log.Printf("Failed to send NodeData to %s: %v", peerID, err)
+	}
+}
+
+func (node *OracleNode) ReceiveNodeData(stream network.Stream) {
+	defer stream.Close()
+
+	jsonData := make([]byte, 1024)
+	n, err := stream.Read(jsonData)
+	if err != nil {
+		log.Printf("Failed to read NodeData from %s: %v", stream.Conn().RemotePeer(), err)
+		return
+	}
+
+	var nodeData []NodeData
+	if err := json.Unmarshal(jsonData[:n], &nodeData); err != nil {
+		log.Printf("Failed to unmarshal NodeData: %v", err)
+		return
+	}
+
+	for _, data := range nodeData {
+		node.NodeTracker.HandleIncomingData(&data)
+	}
+}
+
+func (node *OracleNode) OnJoinEvent(peerID peer.ID) {
+	// Existing join event handling code...
+
+	// Send NodeData to the new node
+	node.SendNodeData(peerID)
 }
