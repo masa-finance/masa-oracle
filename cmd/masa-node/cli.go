a2ccc94a3097bccc5a6e94d165694ce27b92a942
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-12-02 11:15:56 -0800

    Working separation of --stake from start flags. Need to test other flags before merge :+1:

--- cmd/masa-node/cli.go
+++ cmd/masa-node/cli.go
@@ -40,31 +40,28 @@ func init() {
 	flag.StringVar(&configFile, "config", "config.json", "Path to the config file")
 	flag.BoolVar(&start, "start", false, "Start flag")
 	flag.IntVar(&portNbr, "port", getPort("portNbr"), "The port number")
-	flag.BoolVar(&udp, "udp", getEnvAsBool("UDP", false), "UDP flag")
-	flag.BoolVar(&tcp, "tcp", getEnvAsBool("TCP", false), "TCP flag")
+	flag.BoolVar(&udp, "udp", false, "UDP flag") // Default value set to false
+	flag.BoolVar(&tcp, "tcp", false, "TCP flag") // Default value set to false
 	flag.StringVar(&signature, "signature", "", "The signature from the staking contract")
 	flag.StringVar(&flagBootnodes, "bootnodes", "", "Comma-separated list of bootnodes")
 	flag.StringVar(&data, "data", "", "The data to verify the signature against")
 	flag.StringVar(&stakeAmount, "stake", "", "Amount of tokens to stake")
 	flag.Parse()
 
-	// New code to handle staking
+	// Staking logic
 	if stakeAmount != "" {
 		amount, ok := new(big.Int).SetString(stakeAmount, 10)
 		if !ok {
 			logrus.Fatal("Invalid stake amount")
 		}
 
-		// Retrieve the current user's home directory
 		usr, err := user.Current()
 		if err != nil {
 			logrus.Fatal("Failed to get user's home directory:", err)
 		}
 
-		// Construct the path to the private key file within the .masa directory
 		keyFilePath := filepath.Join(usr.HomeDir, ".masa", "masa_oracle_key")
 
-		// Retrieve or create the private key using the GetOrCreatePrivateKey function
 		_, ecdsaPrivateKey, err := masaCrypto.GetOrCreatePrivateKey(keyFilePath)
 		if err != nil {
 			logrus.Fatal(err)
@@ -75,23 +72,32 @@ func init() {
 			logrus.Fatal(err)
 		}
 
-		// Approve the staking contract to spend tokens on behalf of the user
 		approveReceipt, err := stakingClient.Approve(amount)
 		if err != nil {
 			logrus.Fatal("Failed to approve tokens for staking:", err)
 		}
 		logrus.Infof("Approve transaction receipt: %v", approveReceipt)
 
-		// Stake the tokens after approval
 		stakeReceipt, err := stakingClient.Stake(amount)
 		if err != nil {
 			logrus.Fatal("Failed to stake tokens:", err)
 		}
 		logrus.Infof("Stake transaction receipt: %v", stakeReceipt)
+
+		// Exit after staking, do not proceed to start the node
+		os.Exit(0)
 	}
 
 	// Node startup logic
 	if start {
+		// Set the UDP and TCP flags based on environment variables if not already set
+		if !udp {
+			udp = getEnvAsBool("UDP", false)
+		}
+		if !tcp {
+			tcp = getEnvAsBool("TCP", false)
+		}
+
 		if flagBootnodes != "" {
 			bootnodes = flagBootnodes
 		} else {
@@ -107,9 +113,8 @@ func init() {
 			logrus.Error(err)
 		}
 
-		if !udp && !tcp {
-			udp = true
-		}
+		// Additional node startup logic here...
+		// This is where you would start the node
 	}
 }
 
