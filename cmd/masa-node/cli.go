c63803d5a97a084ab39b140a877a38f66559d850
Bob Stevens <35038919+restevens402@users.noreply.github.com>
2023-12-13 15:43:06 -0800

    Revert "Merge branch 'main' into dev"
    This reverts commit 4ca35247e7cf07946f1c0c37861ef39c58977d77, reversing
    changes made to aa3f75dd4442369c4ef573f458fc0752b4a3fbd7.

--- cmd/masa-node/cli.go
+++ cmd/masa-node/cli.go
@@ -28,6 +28,7 @@ var (
 	flagBootnodes string
 	data          string
 	stakeAmount   string
+	debug         bool
 )
 
 func init() {
@@ -41,6 +42,7 @@ func init() {
 	flag.StringVar(&flagBootnodes, "bootnodes", "", "Comma-separated list of bootnodes")
 	flag.StringVar(&data, "data", "", "The data to verify the signature against")
 	flag.StringVar(&stakeAmount, "stake", "", "Amount of tokens to stake")
+	flag.BoolVar(&debug, "debug", false, "Override some protections for debugging (temporary)")
 	flag.Parse()
 
 	if start {
