29f0d9d3e2af8d257f1770d795d3b671172c9020
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2024-01-08 21:18:26 -0800

    Break out approve.go and stake.go to refacrtor contracts.go

--- pkg/staking/contracts.go
+++ pkg/staking/contracts.go
@@ -1,16 +1,10 @@
 package staking
 
 import (
-	"context"
 	"crypto/ecdsa"
 	"fmt"
-	"math/big"
 
-	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -40,101 +34,4 @@ func NewClient(privateKey *ecdsa.PrivateKey) (*Client, error) {
 		EthClient:  client,
 		PrivateKey: privateKey,
 	}, nil
-}
-
-func (sc *Client) Approve(amount *big.Int) (string, error) {
-	parsedABI, err := GetABI("contracts/node_modules/@masa-finance/masa-contracts-oracle/artifacts/contracts/MasaToken.sol/MasaToken.json")
-	if err != nil {
-		return "", err
-	}
-
-	fromAddress := crypto.PubkeyToAddress(sc.PrivateKey.PublicKey)
-	nonce, err := sc.EthClient.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		return "", fmt.Errorf("failed to get nonce: %v", err)
-	}
-
-	value := big.NewInt(0)
-	data, err := parsedABI.Pack("approve", OracleNodeStakingContractAddress, amount)
-	if err != nil {
-		return "", fmt.Errorf("failed to pack data for approve: %v", err)
-	}
-
-	gasPrice, err := sc.EthClient.SuggestGasPrice(context.Background())
-	if err != nil {
-		return "", fmt.Errorf("failed to suggest gas price: %v", err)
-	}
-
-	msg := ethereum.CallMsg{
-		From: fromAddress,
-		To:   &MasaTokenAddress,
-		Data: data,
-	}
-	gasLimit, err := sc.EthClient.EstimateGas(context.Background(), msg)
-	if err != nil {
-		return "", fmt.Errorf("failed to estimate gas: %v", err)
-	}
-
-	tx := types.NewTransaction(nonce, MasaTokenAddress, value, gasLimit, gasPrice, data)
-
-	chainID, err := sc.EthClient.NetworkID(context.Background())
-	if err != nil {
-		return "", fmt.Errorf("failed to get network ID: %v", err)
-	}
-	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), sc.PrivateKey)
-	if err != nil {
-		return "", fmt.Errorf("failed to sign transaction: %v", err)
-	}
-
-	err = sc.EthClient.SendTransaction(context.Background(), signedTx)
-	if err != nil {
-		return "", fmt.Errorf("failed to send transaction: %v", err)
-	}
-
-	receipt, err := bind.WaitMined(context.Background(), sc.EthClient, signedTx)
-	if err != nil {
-		return "", fmt.Errorf("failed to get transaction receipt: %v", err)
-	}
-	if receipt.Status != 1 {
-		return "", fmt.Errorf("transaction failed: %v", receipt)
-	}
-
-	return signedTx.Hash().Hex(), nil
-}
-
-func (sc *Client) Stake(amount *big.Int) (string, error) {
-	chainID, err := sc.EthClient.NetworkID(context.Background())
-	if err != nil {
-		return "", fmt.Errorf("failed to get network ID: %v", err)
-	}
-
-	auth, err := bind.NewKeyedTransactorWithChainID(sc.PrivateKey, chainID)
-	if err != nil {
-		return "", fmt.Errorf("failed to create keyed transactor: %v", err)
-	}
-
-	parsedABI, err := GetABI("contracts/node_modules/@masa-finance/masa-contracts-oracle/artifacts/contracts/OracleNodeStaking.sol/OracleNodeStaking.json")
-	if err != nil {
-		return "", err
-	}
-
-	stakingContract := bind.NewBoundContract(OracleNodeStakingContractAddress, parsedABI, sc.EthClient, sc.EthClient, sc.EthClient)
-	if err != nil {
-		return "", fmt.Errorf("failed to bind staking contract instance: %v", err)
-	}
-
-	tx, err := stakingContract.Transact(auth, "stake", amount)
-	if err != nil {
-		return "", fmt.Errorf("failed to send stake transaction: %v", err)
-	}
-
-	receipt, err := bind.WaitMined(context.Background(), sc.EthClient, tx)
-	if err != nil {
-		return "", fmt.Errorf("failed to get transaction receipt: %v", err)
-	}
-	if receipt.Status != 1 {
-		return "", fmt.Errorf("transaction failed: %v", receipt)
-	}
-
-	return tx.Hash().Hex(), nil
 }
