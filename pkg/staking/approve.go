8798178043be0450f07a9b85a9b1eff8b2db5e4e
Bob Stevens <35038919+restevens402@users.noreply.github.com>
2024-01-09 14:28:20 -0800

    Squashed commit of the following:
    commit dc1ce3eed5989549217eafa18d1adb48f84ecff1
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Tue Jan 9 12:47:31 2024 -0800
    
        Update contract ABI paths and addresses in staking package
    
        This commit updates the `abi.go` file within the staking package to include the latest contract ABI paths and corresponding addresses from the `addresses.json` file. The `ContractAddresses` struct has been synchronized with the new contract information to ensure the staking module interacts correctly with the updated smart contracts.
    
    commit f764b689e359109485df0a5720aae667df937153
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Tue Jan 9 12:42:14 2024 -0800
    
        Update contracts 'yarn upgrade' in /contracts folder and update StakedMasaToken naming
    
    commit fe8b32f057f5596bc41de2a1152c3585247b1651
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Mon Jan 8 21:24:23 2024 -0800
    
        Update config.json
    
    commit 29f0d9d3e2af8d257f1770d795d3b671172c9020
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Mon Jan 8 21:18:26 2024 -0800
    
        Break out approve.go and stake.go to refacrtor contracts.go
    
    commit c63e2b3bbb9997d975399ef2bf1a59b38cc416b9
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Mon Jan 8 21:11:15 2024 -0800
    
        Refactor abi code
    
    commit a7ceb9f6f04ae6eb45c48b879830810b17ff43b7
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Mon Jan 8 20:57:43 2024 -0800
    
        Refactor to add config.go to break out RPC and Contract addresses
    
    commit 484a2df09f2f0dd22a64a44fceb3fb0a429a0bd7
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Mon Jan 8 20:37:16 2024 -0800
    
        Load .env from local .env or export it using export RPC_URL=
    
    commit e05e04a417ec71d4565760bb26dc32d616c59d30
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Mon Jan 8 20:32:38 2024 -0800
    
        separate RPC into .env file
    
    commit 310e488c98068071a69d7da83133fa90985eeff6
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Mon Jan 8 20:25:28 2024 -0800
    
        First refactor of contracts.go - tested working with ABI imports
    
    commit 29bfbd64b379308d07c7b942cfee002fe9a61b58
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Mon Jan 8 16:49:10 2024 -0800
    
        feat: Load contract addresses dynamically from JSON
    
        feat: Load contract addresses dynamically from JSON
    
        This update modifies the staking client to dynamically load contract
        addresses from an external JSON file instead of using hardcoded values.
        The NewClient function now calls LoadContractAddresses to set the global
        variables MasaTokenAddress and OracleNodeStakingContractAddress. This
        enhancement facilitates easier configuration changes and promotes best
        practices for maintainable code.
    
        - Add LoadContractAddresses function to read addresses from JSON
        - Update NewClient to use dynamic addresses for contract interaction
        - Ensure proper error handling for file reading and JSON unmarshalling
    
    commit f8ad790d79952131338efb8d84238f9ec54a7054
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Mon Jan 8 16:39:25 2024 -0800
    
        init yarn and add contracts
    
    commit 99d21e1dca90e3ef8ba4fb31b6181591fddbbbd1
    Author: Brendan Playford <34052452+teslashibe@users.noreply.github.com>
    Date:   Mon Jan 8 16:33:50 2024 -0800
    
        delete old contracts

---
+++ pkg/staking/approve.go
@@ -0,0 +1,73 @@
+// approve.go
+package staking
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func (sc *Client) Approve(amount *big.Int) (string, error) {
+	parsedABI, err := GetABI(MasaTokenABIPath)
+	if err != nil {
+		return "", err
+	}
+
+	fromAddress := crypto.PubkeyToAddress(sc.PrivateKey.PublicKey)
+	nonce, err := sc.EthClient.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return "", fmt.Errorf("failed to get nonce: %v", err)
+	}
+
+	value := big.NewInt(0)
+	data, err := parsedABI.Pack("approve", OracleNodeStakingContractAddress, amount)
+	if err != nil {
+		return "", fmt.Errorf("failed to pack data for approve: %v", err)
+	}
+
+	gasPrice, err := sc.EthClient.SuggestGasPrice(context.Background())
+	if err != nil {
+		return "", fmt.Errorf("failed to suggest gas price: %v", err)
+	}
+
+	msg := ethereum.CallMsg{
+		From: fromAddress,
+		To:   &MasaTokenAddress,
+		Data: data,
+	}
+	gasLimit, err := sc.EthClient.EstimateGas(context.Background(), msg)
+	if err != nil {
+		return "", fmt.Errorf("failed to estimate gas: %v", err)
+	}
+
+	tx := types.NewTransaction(nonce, MasaTokenAddress, value, gasLimit, gasPrice, data)
+
+	chainID, err := sc.EthClient.NetworkID(context.Background())
+	if err != nil {
+		return "", fmt.Errorf("failed to get network ID: %v", err)
+	}
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), sc.PrivateKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign transaction: %v", err)
+	}
+
+	err = sc.EthClient.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		return "", fmt.Errorf("failed to send transaction: %v", err)
+	}
+
+	receipt, err := bind.WaitMined(context.Background(), sc.EthClient, signedTx)
+	if err != nil {
+		return "", fmt.Errorf("failed to get transaction receipt: %v", err)
+	}
+	if receipt.Status != 1 {
+		return "", fmt.Errorf("transaction failed: %v", receipt)
+	}
+
+	return signedTx.Hash().Hex(), nil
+}
--- pkg/staking/verify.go
+++ pkg/staking/verify.go
@@ -2,82 +2,52 @@ package staking
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
 	"math/big"
-	"os"
-	"strings"
 
 	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-const (
-	infuraURL       = "https://rpc.sepolia.org"                    // update to Sepolia - this should be added as an environment variable sometime
-	contractAddress = "0xd925bc5d3eCd899a3F7B8D762397D2DC75E1187b" // this is the sepolia contract address
-)
-
-type Contract struct {
-	ABI []interface{} `json:"abi"`
-}
-
-func getContractABI() []interface{} {
-	jsonFile, err := os.ReadFile("contracts/build/contracts/OracleNodeStakingContract.json")
-	if err != nil {
-		log.Fatalf("Failed to read contract JSON: %v", err)
-	}
-
-	var contract Contract
-	err = json.Unmarshal(jsonFile, &contract)
-	if err != nil {
-		log.Fatalf("Failed to unmarshal contract JSON: %v", err)
-	}
-
-	return contract.ABI
-}
-
 func VerifyStakingEvent(userAddress string) (bool, error) {
-	client, err := ethclient.Dial(infuraURL)
+	rpcURL := GetRPCURL()
+	client, err := ethclient.Dial(rpcURL)
 	if err != nil {
-		return false, errors.New(fmt.Sprintf("Failed to connect to the Ethereum client: %v", err))
+		return false, fmt.Errorf("Failed to connect to the Ethereum client: %v", err)
 	}
 
-	contractABI := getContractABI()
-	abiJSON, err := json.Marshal(contractABI)
+	parsedABI, err := GetABI(OracleNodeStakingABIPath) // Use the GetABI function from abi.go
 	if err != nil {
-		return false, errors.New(fmt.Sprintf("Failed to marshal contract ABI: %v", err))
-	}
-	parsedABI, err := abi.JSON(strings.NewReader(string(abiJSON)))
-	if err != nil {
-		return false, errors.New(fmt.Sprintf("Failed to parse contract ABI: %v", err))
+		return false, err
 	}
 
 	address := common.HexToAddress(userAddress)
 	stake, err := parsedABI.Pack("stakes", address)
 	if err != nil {
-		return false, errors.New(fmt.Sprintf("Failed to pack data for stakes call: %v", err))
+		return false, fmt.Errorf("Failed to pack data for stakes call: %v", err)
 	}
 
-	// Address correction
-	contractAddr := common.HexToAddress(contractAddress)
+	addresses, err := LoadContractAddresses()
+	if err != nil {
+		return false, fmt.Errorf("Failed to load contract addresses: %v", err)
+	}
+	contractAddr := common.HexToAddress(addresses.Sepolia.OracleNodeStaking)
 
 	callMsg := ethereum.CallMsg{
-		To:   &contractAddr, // Adjusted to use a pointer
+		To:   &contractAddr,
 		Data: stake,
 	}
 
 	result, err := client.CallContract(context.Background(), callMsg, nil)
 	if err != nil {
-		return false, errors.New(fmt.Sprintf("Failed to call stakes function: %v", err))
+		return false, fmt.Errorf("Failed to call stakes function: %v", err)
 	}
 
 	stakesAmountInterfaces, err := parsedABI.Unpack("stakes", result)
 	if err != nil {
-		return false, errors.New(fmt.Sprintf("Failed to unpack stakes: %v", err))
+		return false, fmt.Errorf("Failed to unpack stakes: %v", err)
 	}
 
 	stakesAmount, ok := stakesAmountInterfaces[0].(*big.Int)
