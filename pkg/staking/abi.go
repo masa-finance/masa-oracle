b64a2a0e98db721cc3532bac22d5d90242f5b13e
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2024-01-09 12:47:31 -0800

    Update contract ABI paths and addresses in staking package
    This commit updates the `abi.go` file within the staking package to include the latest contract ABI paths and corresponding addresses from the `addresses.json` file. The `ContractAddresses` struct has been synchronized with the new contract information to ensure the staking module interacts correctly with the updated smart contracts.

--- pkg/staking/abi.go
+++ pkg/staking/abi.go
@@ -12,12 +12,17 @@ import (
 
 const (
 	MasaTokenABIPath         = "contracts/node_modules/@masa-finance/masa-contracts-oracle/artifacts/contracts/MasaToken.sol/MasaToken.json"
+	NodeDataMetricsABIPath   = "contracts/node_modules/@masa-finance/masa-contracts-oracle/artifacts/contracts/NodeDataMetrics.sol/NodeDataMetrics.json"
+	NodeRewardPoolABIPath    = "contracts/node_modules/@masa-finance/masa-contracts-oracle/artifacts/contracts/NodeRewardPool.sol/NodeRewardPool.json"
 	OracleNodeStakingABIPath = "contracts/node_modules/@masa-finance/masa-contracts-oracle/artifacts/contracts/OracleNodeStaking.sol/OracleNodeStaking.json"
+	StakedMasaTokenABIPath   = "contracts/node_modules/@masa-finance/masa-contracts-oracle/artifacts/contracts/StakedMasaToken.sol/StakedMasaToken.json"
 )
 
 type ContractAddresses struct {
 	Sepolia struct {
 		MasaToken         string `json:"MasaToken"`
+		NodeDataMetrics   string `json:"NodeDataMetrics"`
+		NodeRewardPool    string `json:"NodeRewardPool"`
 		OracleNodeStaking string `json:"OracleNodeStaking"`
 		StakedMasaToken   string `json:"StakedMasaToken"`
 	} `json:"sepolia"`
