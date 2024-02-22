3da8624ae14c3466f820c9dae617abbe1841bc6e
Bob Stevens <35038919+restevens402@users.noreply.github.com>
2024-02-21 19:35:05 -0800

    Replace flag package with pflag in cli.go
    The standard flag package has been replaced with spf13/pflag for command line flag parsing in cli.go. This change supports the capability of defining POSIX compliant flags and binds command line flags with Viper, allowing for more granular flag handling and flag precedence. Also, debug logs are introduced to aid in debugging.

--- cmd/masa-node/cli.go
+++ cmd/masa-node/cli.go
@@ -2,13 +2,9 @@
 package main
 
 import (
-	"flag"
-	"os"
-
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
-
-	masa "github.com/masa-finance/masa-oracle/pkg"
 )
 
 var (
@@ -25,24 +21,25 @@ var (
 
 func init() {
 	// Define flags
-	flag.IntVar(&portNbr, "port", viper.GetInt("PORT_NBR"), "The port number")
-	flag.BoolVar(&udp, "udp", viper.GetBool("UDP"), "UDP flag") // Default value set to false
-	flag.BoolVar(&tcp, "tcp", viper.GetBool("TCP"), "TCP flag") // Default value set to false
-	flag.StringVar(&signature, "signature", "", "The signature from the staking contract")
-	flag.StringVar(&bootnodes, "bootnodes", viper.GetString("BOOTNODES"), "Comma-separated list of bootnodes")
-	flag.StringVar(&data, "data", "", "The data to verify the signature against")
-	flag.StringVar(&stakeAmount, "stake", viper.GetString("STAKE_AMOUNT"), "Amount of tokens to stake")
-	flag.BoolVar(&debug, "debug", viper.GetBool("LOG_LEVEL"), "Override some protections for debugging (temporary)")
-	flag.StringVar(&env, "env", viper.GetString("ENV"), "Environment to connect to")
-	flag.Parse()
-
-	err := os.Setenv(masa.Environment, env)
+	pflag.IntVar(&portNbr, "port", viper.GetInt("PORT_NBR"), "The port number")
+	pflag.BoolVar(&udp, "udp", viper.GetBool("UDP"), "UDP flag") // Default value set to false
+	pflag.BoolVar(&tcp, "tcp", viper.GetBool("TCP"), "TCP flag") // Default value set to false
+	pflag.StringVar(&signature, "signature", "", "The signature from the staking contract")
+	pflag.StringVar(&bootnodes, "bootnodes", viper.GetString("BOOTNODES"), "Comma-separated list of bootnodes")
+	pflag.StringVar(&data, "data", "", "The data to verify the signature against")
+	pflag.StringVar(&stakeAmount, "stake", viper.GetString("STAKE_AMOUNT"), "Amount of tokens to stake")
+	pflag.BoolVar(&debug, "debug", viper.GetBool("LOG_LEVEL"), "Override some protections for debugging (temporary)")
+	pflag.StringVar(&env, "env", viper.GetString("ENV"), "Environment to connect to")
+	pflag.Parse()
+	err := viper.BindPFlags(pflag.CommandLine)
 	if err != nil {
-		logrus.Error(err)
+		return
 	}
-	err = os.Setenv(masa.Peers, bootnodes)
-	if err != nil {
-		logrus.Error(err)
+	if debug {
+		logrus.Debugf("Direct flag value: %s", env)
+		for key, val := range viper.AllSettings() {
+			logrus.Debugf("%s: %v", key, val)
+		}
+		logrus.Debugf("ENV from Viper: %s", viper.Get("ENV"))
 	}
-
 }
