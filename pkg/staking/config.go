b371ed6f1f8725a03580a2f139102cdd38da4ac2
JD <156010594+5u6r054@users.noreply.github.com>
2024-02-26 10:52:55 -0800

    updates to rpc_url var refs

--- pkg/staking/config.go
+++ pkg/staking/config.go
@@ -2,20 +2,17 @@ package staking
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/spf13/viper"
-
-	masa "github.com/masa-finance/masa-oracle/pkg"
 )
 
 func GetRPCURL() (string, error) {
-	url := viper.GetString(masa.RpcUrl)
+	url := viper.GetString("RPC_URL")
 	if url == "" {
-		return "", errors.New(fmt.Sprintf("%s environment variable is not set", masa.RpcUrl))
+		return "", fmt.Errorf("%s environment variable is not set", "RPC_URL")
 	}
 	return url, nil
 }
