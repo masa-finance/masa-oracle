1a18bab70f79aad607d0046f7d580e648719540f
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2024-02-28 21:38:43 -0800

    Delete unused code

--- pkg/cryptoInterfaces/interfaces.go
+++
@@ -1,9 +0,0 @@
-package cryptoInterfaces
-
-import "github.com/libp2p/go-libp2p/core/crypto"
-
-// KeyLoader defines the interface for loading private and public keys.
-type KeyLoader interface {
-	LoadPrivKey() (crypto.PrivKey, error)
-	LoadPubKey() (crypto.PubKey, error)
-}
