d38a9f667531227ee51b6ffaad7210960c7f5f03
jdutchak <john@dutchak.com>
2024-07-18 10:17:18 -0400

    restructure of dht SharedData with ttl

--- pkg/db/types.go
+++ pkg/db/types.go
@@ -2,3 +2,9 @@ package db
 
 // SharedData is a map that stores shared data across the application.
 type SharedData map[string]interface{}
+
+// type SharedData struct {
+// 	Key   string      `json:"key"`
+// 	Value interface{} `json:"value"`
+// 	TTL   time.Time   `json:"ttl"`
+// }
