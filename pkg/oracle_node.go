0464cc24282eac683edf50bb1fcf8bfbb2b4cd32
jdutchak <john@dutchak.com>
2024-07-24 18:26:37 -0400

    blockeventtracker

--- pkg/oracle_node.go
+++ pkg/oracle_node.go
@@ -392,22 +392,9 @@ func (b *BlockEventTracker) HandleMessageOld(m *pubsub.Message) {
 		logrus.Errorf("[-] Failed to unmarshal message: %v", err)
 		return
 	}
-
 	b.mu.Lock()
-	defer b.mu.Unlock()
-
-	switch v := blockEvents.(type) {
-	case []BlockEvents:
-		b.BlockEvents = append(b.BlockEvents, v...)
-	case BlockEvents:
-		b.BlockEvents = append(b.BlockEvents, v)
-	case map[string]interface{}:
-		b.BlockEvents = append(b.BlockEvents, BlockEvents{})
-	default:
-		logrus.Errorf("[-] Unexpected data type in message %+v", v)
-		return
-	}
-
+	b.BlockEvents = append(b.BlockEvents, blockEvents)
+	b.mu.Unlock()
 	blocksCh <- m
 }
 
