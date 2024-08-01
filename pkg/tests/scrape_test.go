0caafebcdf949493dd832c8db3cc2f360075e2bb
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2024-08-01 11:36:24 -0400

    fix: linting in scrape_test.go

--- pkg/tests/scrape_test.go
+++ pkg/tests/scrape_test.go
@@ -162,7 +162,7 @@ func TestScrapeTweetsWithSentimentByQuery(t *testing.T) {
 	twitterScraperTweets := make([]*twitterscraper.TweetResult, len(deserializedTweets))
 	for i, tweet := range deserializedTweets {
 		twitterScraperTweets[i] = &twitterscraper.TweetResult{
-			Tweet: tweet,
+			Tweet: *tweet,
 			Error: nil,
 		}
 	}
