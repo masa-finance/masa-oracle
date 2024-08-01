d0932702f06d06c7ebc28c08e21b8b3168696ecb
Chris Schroeder <32779673+theMultitude@users.noreply.github.com>
2024-08-01 18:48:08 -0400

    feat: add Discord Message Functionality

--- pkg/workers/workers.go
+++ pkg/workers/workers.go
@@ -123,9 +123,11 @@ func (a *Worker) Receive(ctx actor.Context) {
 		}
 	case *messages.Work:
 		if a.Node.IsWorker() {
+			logrus.Infof("[+] Received Work message: %+v", m)
 			a.HandleWork(ctx, m, a.Node)
 		}
 	case *messages.Response:
+		logrus.Infof("[+] Received Response message: %+v", m)
 		msg := &pubsub2.Message{}
 		err := json.Unmarshal([]byte(m.Value), msg)
 		if err != nil {
@@ -138,7 +140,7 @@ func (a *Worker) Receive(ctx actor.Context) {
 		workerDoneCh <- msg
 		ctx.Poison(ctx.Self())
 	default:
-		logrus.Warningf("[+] Received unknown message: %T", m)
+		logrus.Warningf("[+] Received unknown message: %T, message: %+v", m, m)
 	}
 }
 
@@ -284,7 +286,7 @@ func SendWork(node *masa.OracleNode, m *pubsub2.Message) {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			future := node.ActorEngine.RequestFuture(pid, message, 30*time.Second)
+			future := node.ActorEngine.RequestFuture(pid, message, 60*time.Second) // Increase timeout from 30 to 60 seconds
 			result, err := future.Result()
 			if err != nil {
 				logrus.Errorf("[-] Error receiving response from local worker: %v", err)
