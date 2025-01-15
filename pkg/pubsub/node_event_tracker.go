8446df438abc405dee32179a1cb862dd7ca0f9ae
Alvin Reyes <me@areyes.onl>
2025-01-15 11:54:34 -0500

    fix: check node and remote peer deref #660 (#665)
    * fix: check node reference and remote peer reference before accessing them to prevent nil deref error
    
    * fix: fmt issues
    
    * use address string on logs instead of accessing the instance
    
    * check if the network is nil
    
    * reverted the info message
    
    * add unit test for node data
    
    * add unit test for node event tracker
    
    * add more unit tests
    
    * add more unit test to increase coverage
    
    * add more unit test to increase coverage
    
    ---------
    
    Co-authored-by: Alvin Reyes <alvinreyes@mac.home>

--- pkg/pubsub/node_event_tracker.go
+++ pkg/pubsub/node_event_tracker.go
@@ -135,7 +135,20 @@ func (net *NodeEventTracker) ListenClose(n network.Network, a ma.Multiaddr) {
 // saves the updated nodeData.
 func (net *NodeEventTracker) Connected(n network.Network, c network.Conn) {
 	// A node has joined the network
+	logrus.Debug("Connected")
+	if net == nil {
+		logrus.Error("[-] Network is nil")
+		return
+	}
+
+	// check if the connection is nil
+	if c == nil {
+		logrus.Error("[-] Connection is nil")
+		return
+	}
 	remotePeer := c.RemotePeer()
+
+	// get the peer ID of the remote peer
 	peerID := remotePeer.String()
 
 	nodeData, exists := net.nodeData.Get(peerID)
@@ -155,10 +168,18 @@ func (net *NodeEventTracker) Connected(n network.Network, c network.Conn) {
 			}
 		}
 	}
+
+	// prevent panic
+	defer func() {
+		if r := recover(); r != nil {
+			logrus.Errorf("Recovered from panic: %v", r)
+		}
+	}()
+
 	logrus.WithFields(logrus.Fields{
-		"Peer":    c.RemotePeer().String(),
-		"network": n,
-		"conn":    c,
+		"peer_id": fmt.Sprintf("%v", peerID),
+		"network": fmt.Sprintf("%v", n),
+		"conn":    fmt.Sprintf("%v", c),
 	}).Info("[+] Connected")
 }
 
@@ -170,6 +191,12 @@ func (net *NodeEventTracker) Connected(n network.Network, c network.Conn) {
 func (net *NodeEventTracker) Disconnected(n network.Network, c network.Conn) {
 	logrus.Debug("Disconnect")
 
+	// check if net is nil
+	if net == nil {
+		logrus.Error("[-] Network is nil")
+		return
+	}
+
 	peerID := c.RemotePeer().String()
 
 	nodeData, exists := net.nodeData.Get(peerID)
@@ -179,8 +206,8 @@ func (net *NodeEventTracker) Disconnected(n network.Network, c network.Conn) {
 		logrus.Debugf("Node data does not exist for disconnected node: %s", peerID)
 		return
 	}
-	buffered := net.ConnectBuffer[peerID]
-	if buffered.NodeData != nil {
+	buffered, exists := net.ConnectBuffer[peerID]
+	if exists && buffered.NodeData != nil {
 		buffered.NodeData.Left()
 		delete(net.ConnectBuffer, peerID)
 		buffered.NodeData.Joined(net.nodeVersion)
@@ -189,11 +216,19 @@ func (net *NodeEventTracker) Disconnected(n network.Network, c network.Conn) {
 		nodeData.Left()
 		net.NodeDataChan <- nodeData
 	}
+
+	// prevent panic
+	defer func() {
+		if r := recover(); r != nil {
+			logrus.Errorf("Recovered from panic: %v", r)
+		}
+	}()
+
 	logrus.WithFields(logrus.Fields{
-		"Peer":    c.RemotePeer().String(),
-		"network": n,
-		"conn":    c,
-	}).Info("[+] Disconnected")
+		"peer_id": fmt.Sprintf("%v", peerID),
+		"network": fmt.Sprintf("%v", n),
+		"conn":    fmt.Sprintf("%v", c),
+	}).Info("[+] Connected")
 }
 
 // HandleMessage unmarshals the received pubsub message into a NodeData struct,
