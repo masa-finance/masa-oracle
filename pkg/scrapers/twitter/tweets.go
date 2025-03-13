6633e75562b7b35c1162bcc92c92cd27bbc556be
Grant Foster <grantdfoster@gmail.com>
2025-03-13 11:18:44 -0700

    fix: makes data types more generic for tweet id

--- pkg/scrapers/twitter/tweets.go
+++ pkg/scrapers/twitter/tweets.go
@@ -2,18 +2,88 @@ package twitter
 
 import (
 	"context"
+	"time"
 
 	twitterscraper "github.com/imperatrona/twitter-scraper"
 
 	data_types "github.com/masa-finance/masa-oracle/pkg/workers/types"
 )
 
+type SimpleTweet struct {
+	ConversationID    string
+	GIFs              []twitterscraper.GIF
+	Hashtags          []string
+	HTML              string
+	ID                string
+	InReplyToStatusID string
+	IsQuoted          bool
+	IsPin             bool
+	IsReply           bool
+	IsRetweet         bool
+	IsSelfThread      bool
+	Likes             int
+	Name              string
+	Mentions          []twitterscraper.Mention
+	PermanentURL      string
+	Photos            []twitterscraper.Photo
+	Place             *twitterscraper.Place
+	QuotedStatusID    string
+	Replies           int
+	Retweets          int
+	RetweetedStatusID string
+	Text              string
+	TimeParsed        time.Time
+	Timestamp         int64
+	URLs              []string
+	UserID            string
+	Username          string
+	Videos            []twitterscraper.Video
+	Views             int
+	SensitiveContent  bool
+}
+
 type TweetResult struct {
-	Tweet *twitterscraper.Tweet
+	Tweet *SimpleTweet
 	Error error
 }
 
-func ScrapeTweetByID(id string) (*twitterscraper.Tweet, *data_types.LoginEvent, error) {
+// Conversion function
+func convertToSimpleTweet(tweet *twitterscraper.Tweet) *SimpleTweet {
+	return &SimpleTweet{
+		ConversationID:    tweet.ConversationID,
+		GIFs:              tweet.GIFs,
+		Hashtags:          tweet.Hashtags,
+		HTML:              tweet.HTML,
+		ID:                tweet.ID,
+		InReplyToStatusID: tweet.InReplyToStatusID,
+		IsQuoted:          tweet.IsQuoted,
+		IsPin:             tweet.IsPin,
+		IsReply:           tweet.IsReply,
+		IsRetweet:         tweet.IsRetweet,
+		IsSelfThread:      tweet.IsSelfThread,
+		Likes:             tweet.Likes,
+		Name:              tweet.Name,
+		Mentions:          tweet.Mentions,
+		PermanentURL:      tweet.PermanentURL,
+		Photos:            tweet.Photos,
+		Place:             tweet.Place,
+		QuotedStatusID:    tweet.QuotedStatusID,
+		Replies:           tweet.Replies,
+		Retweets:          tweet.Retweets,
+		RetweetedStatusID: tweet.RetweetedStatusID,
+		Text:              tweet.Text,
+		TimeParsed:        tweet.TimeParsed,
+		Timestamp:         tweet.Timestamp,
+		URLs:              tweet.URLs,
+		UserID:            tweet.UserID,
+		Username:          tweet.Username,
+		Videos:            tweet.Videos,
+		Views:             tweet.Views,
+		SensitiveContent:  tweet.SensitiveContent,
+	}
+}
+
+func ScrapeTweetByID(id string) (*SimpleTweet, *data_types.LoginEvent, error) {
 	scraper, account, loginEvent, err := getAuthenticatedScraper()
 	if err != nil {
 		return nil, loginEvent, err
@@ -26,7 +96,7 @@ func ScrapeTweetByID(id string) (*twitterscraper.Tweet, *data_types.LoginEvent, 
 		}
 		return nil, loginEvent, err
 	}
-	return tweet, loginEvent, nil
+	return convertToSimpleTweet(tweet), loginEvent, nil
 }
 
 func ScrapeTweetsByQuery(query string, count int) ([]*TweetResult, *data_types.LoginEvent, error) {
@@ -45,7 +115,7 @@ func ScrapeTweetsByQuery(query string, count int) ([]*TweetResult, *data_types.L
 			}
 			return nil, loginEvent, tweet.Error
 		}
-		tweets = append(tweets, &TweetResult{Tweet: &tweet.Tweet})
+		tweets = append(tweets, &TweetResult{Tweet: convertToSimpleTweet(&tweet.Tweet)})
 	}
 	return tweets, loginEvent, nil
 }
